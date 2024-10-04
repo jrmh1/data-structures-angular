8360af5ef65cbe311e06b420bfda65d8518f09b3
Kim <kmjrmh17@gmail.com>
2024-10-05 07:15:46 +0800

    #9 Product List

---
+++ src/app/components/product-list/product-list.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-product-list',
+  templateUrl: './product-list.component.html',
+  styleUrl: './product-list.component.css'
+})
+export class ProductListComponent {
+  listTitle: string = 'Product List';
+  items: string[] = ['Laptop', 'Smartphone', 'Tablet'];
+
+  newItem: string = '';
+
+  addItem() {
+    if (this.newItem.trim()) {
+      this.items.push(this.newItem.trim());
+      this.newItem = '';
+    }
+  }
+}
